f82572820dccfc77f5598c62358e4396d00d7553
parmanand <parmanandmaurya1999@.com>
2023-03-07 18:31:25 +0530

    working-on-reducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 
+import { createContext, useReducer } from 'react';
 import './App.css';
 import Homepage from './Componant/Homepage';
 import './Componant/Homepage.css'
-import './Componant/TagepageForm.css'
+import './Componant/TagepageForm.css';
 
+export const TodoInput = createContext();
+
+const initialstate = {
+  inputOpen: false,
+  donetask: true,
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "InputOpen":
+      return {
+        ...state,
+        inputOpen: true
+      }
+
+    case "Cancel":
+      return {
+        ...state,
+        inputOpen: false
+      }
+    case "donetask": {
+      return {
+        ...state,
+        // taskdone: true
+      }
+    }
+    default: {
+      return state;
+    }
+
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialstate);
   return (
     <div>
-      <Homepage />
+      <TodoInput.Provider value={{ ...state, dispatch: dispatch }}>
+        <Homepage />
+      </TodoInput.Provider>
 
     </div>
   );
 }
-
 export default App;
--- src/Componant/Homepage.js
+++ src/Componant/Homepage.js
@@ -1,34 +1,40 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react';
+import { TodoInput } from '../App';
 import Sidebar from './Sidebar'
 import TagepageForm from './TagepageForm'
-import TodoBody from './TodoBody'
+// import TodoBody from './TodoBody'
 
 
 
 function Homepage() {
-    const [done, setdone] = useState(true)
-    const [popup, setPopup] = useState(false)
+    const appContext = useContext(TodoInput);
+    // const [done, setdone] = useState(true)
+    // // const [popup, setPopup] = useState(false)
 
-    function donehandler() {
-        setdone((pre => !pre))
-    }
-    function pophandler() {
-        setPopup((pre => !pre))
-    }
+    // function donehandler() {
+    //     // setdone((pre => !pre))
+    // }
+    // function pophandler() {
+    //     // setPopup((pre => !pre))
+    // }
 
     return (
         <>
          
-                {popup ? <TagepageForm  popup1={setPopup} /> : <></>}
+                { appContext.inputOpen ? <TagepageForm/> : ""}
                 <div className='maindiv0'>
                     < div className='maindiv'>
                         <div className='nav d-flex justify-content-between '>
                             <div className='navtodo '><h2>todo</h2></div>
-                            <button className='navplush ' onClick={pophandler}><h2>+</h2></button>
-                        </div>
+                        <button className='navplush ' onClick={()=>appContext.dispatch({type: "InputOpen"})}><h2>+</h2></button>
+                    </div>
+                    
+
                         <div className='sidetodo d-flex '>
-                            <Sidebar done1={done} donehandler={donehandler} />
-                            <TodoBody />
+                        <Sidebar
+                            // done1={done} donehandler={donehandler}
+                        />
+                            {/* <TodoBody /> */}
                         </div>
                     </div>
                 </div>
