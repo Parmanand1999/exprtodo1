d37c95f4a2bb6269563b96b17db63c45fb0464fe
parmanand <parmanandmaurya1999@.com>
2023-03-10 18:38:14 +0530

    working_on_todo

--- src/Componant/TodoBody.js
+++ src/Componant/TodoBody.js
@@ -3,37 +3,40 @@ import { TodoInput } from '../App'
 
 function TodoBody() {
     const todoBody = useContext(TodoInput)
-
+    // console.log(todoBody +".............")
     return (
         <>
             < div className=' maindivtag d-flex ' >
-                {/* {stdata.map((item, index) => { */}
-                <div className='titledesdiv '
-                // key={index}
-                >
-                    <div className='tagbtn1 d-flex m-2 justify-content-around'>
-                        <h6>The first task title</h6>
-                        {/* <h6>{item.title}</h6> */}
-                        <button className='editdeletbtn'>
-                            <h3 className='editdeletbtn2'>...</h3>
-                        </button>
-                    </div>
-                    <div>
-                        <p>lorem vdbfgb dfvfd vdfvdf rwfgtw ghb  </p>
-                        {/* <p>{item.discription}</p> */}
-                    </div>
-                    <div className='d-flex justify-content-between mb-2 '>
-                        <div className='box d-flex mx-3'>
-                            <button className='work1 '></button>
-                            <button className='study1 '></button>
-                            <button className='entertain1'></button>
-                            <button className='faimly1'></button>
+                {todoBody.titlediscription.map((item, index) =>
+
+                    <div className='titledesdiv '
+                        key={index}
+                    >
+                        <div className='tagbtn1 d-flex  justify-content-around'>
+
+                            <h6>{item.title}</h6>
+                            <button className='editdeletbtn'>
+                                <h3 className='editdeletbtn2' onClick={()=>todoBody.dispatch({type:"EDIT_DELETE"})}>...</h3>
+                               {todoBody.EDIT_DELETE ? <div className='editedeletebtn'> <button type="button" className="edbtn">Edit</button><br />
+                                    <button className="edbtn ">Delete</button>
+                                </div>:""}
+                            </button>
                         </div>
-                        <div className='taskdone '><input type="checkbox" ></input><label>Done</label>
+                        <div>
+                            <p>{item.discription}</p>
+                        </div>
+                        <div className='d-flex justify-content-between mb-2 '>
+                            <div className='box d-flex mx-3'>
+                                <button className='work1 '></button>
+                                <button className='study1 '></button>
+                                <button className='entertain1'></button>
+                                <button className='faimly1'></button>
+                            </div>
+                            <div className='taskdone '><input type="checkbox" ></input><label>Done</label>
+                            </div>
                         </div>
                     </div>
-                </div>
-                {/* })} */}
+                )}
             </div >
         </>
     )
